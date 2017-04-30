f930d756cd2b7a356bf4e89bf69d8912bc466627
olikk <h_o@bk.ru>
2017-05-01 01:27:52 +0300

    ArraysProbe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Random random = new Random();
+        int[] myArray = new int[random.nextInt(30) + 10];
+        System.out.println("lenth= " + myArray.length);
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = random.nextInt(100);
+            System.out.println("i= " + myArray[i]);
+        }
+
+
     }
 }
